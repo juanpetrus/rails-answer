1a9d84d9c5bc3fdcc60e3a9df60786fb0e15cb52
Juan Petrus <juan.petruss@gmail.com>
2019-06-17 08:14:38 -0400

    Ativando o i18n para o Devise

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -3,3 +3,6 @@ I18n.available_locales = [:en, 'pt-BR']
  
 # Set default locale to something other than :en
 I18n.default_locale = 'pt-BR'
+
+# config/initializers/locale.rb
+#I18n.default_locale = :en
