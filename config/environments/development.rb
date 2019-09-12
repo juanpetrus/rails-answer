5369d0558d61822c712db9b54718647631465b75
Juan Petrus <juan.petruss@gmail.com>
2019-09-12 06:25:42 -0400

    answer ajustes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  
+
   # Devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
